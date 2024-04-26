b8601c6b8b4c3f8c5bd2662e86d529ae92a8d189
Ana Schmidt <122480323+ANA-FAST26@users.noreply.github.com>
2024-04-26 16:47:28 -0300

    Add files via upload

---
+++ Trabalho1/src/Medalha.java
@@ -0,0 +1,15 @@
+public class Medalha {
+
+	private int codigo;
+
+	private int tipo;
+
+	private boolean individual;
+
+	private String modalidade;
+
+	public void adicionaAtleta(Atleta atleta) {
+
+	}
+
+}
